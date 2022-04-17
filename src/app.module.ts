c9c44b6c1f6909a90507870b319ee0b75b12a809
David Sánchez <deividsanchez96@hotmail.com>
2022-04-17 13:18:08 -0500

    Add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, MikroOrmModule.forRoot()],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MikroOrmModule.forRoot(),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
