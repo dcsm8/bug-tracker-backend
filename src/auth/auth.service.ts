de786bdcf84627009196f4d205cdf936d352f3be
David Sánchez <deividsanchez96@hotmail.com>
2022-04-18 00:20:15 -0500

    Add bcryptjs hash compare password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,9 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-    if (user && user.password === password) {
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (user && validPassword) {
       const { password, ...result } = user;
       return result;
     }
