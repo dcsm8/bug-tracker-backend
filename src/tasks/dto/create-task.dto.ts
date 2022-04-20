4728dd5d348c89c6f11e69238a83580461a36943
David Sánchez <deividsanchez96@hotmail.com>
2022-04-20 00:53:49 -0500

    Add create-task dto properties

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { CategoryType } from '../enums/category-type.enum';
+import { NotificationStatusType } from '../enums/notification-status-type.enum';
+import { PriorityType } from '../enums/priority-type.enum';
+import { StatusType } from '../enums/status-type.enum';
+
+export class CreateTaskDto {
+  readonly title: string;
+  readonly priority: PriorityType;
+  readonly status: StatusType;
+  readonly notificationStatus: NotificationStatusType;
+  readonly category: CategoryType;
+  readonly shortDescription: string;
+  readonly longDescription?: string;
+  readonly release?: string;
+}
