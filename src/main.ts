ffbdfac6b71d7575442d642ddeecc6b3b1945d17
David Sánchez <deividsanchez96@hotmail.com>
2022-04-19 23:58:04 -0500

    Add Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bug Tracker')
+    .setDescription(
+      'Record, report, and monitor the bugs in a software development project',
+    )
+    .setVersion('1.0')
+    .addTag('Bug Tracker')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 
