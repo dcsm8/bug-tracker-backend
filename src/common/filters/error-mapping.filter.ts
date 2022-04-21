ca3dee25529652580288f76adb239beb942ebe35
David Sánchez <deividsanchez96@hotmail.com>
2022-04-21 01:58:57 -0500

    Remove unnecessary error items

--- src/common/filters/error-mapping.filter.ts
+++ src/common/filters/error-mapping.filter.ts
@@ -1,12 +1,4 @@
-import {
-  ArgumentsHost,
-  BadRequestException,
-  Catch,
-  ConflictException,
-  InternalServerErrorException,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ArgumentsHost, Catch, NotFoundException } from '@nestjs/common';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
 import { BaseExceptionFilter } from '@nestjs/core';
 import {
@@ -18,51 +10,7 @@ type HttpExceptionConstructor = (object: HttpExceptionObject) => HttpException;
 
 const mapOfHedgeDocErrorsToHttpErrors: Map<string, HttpExceptionConstructor> =
   new Map([
-    // MikroORM
     ['NotFoundError', (object): HttpException => new NotFoundException(object)],
-    // Optional
-    ['NotInDBError', (object): HttpException => new NotFoundException(object)],
-    [
-      'AlreadyInDBError',
-      (object): HttpException => new ConflictException(object),
-    ],
-    [
-      'ForbiddenIdError',
-      (object): HttpException => new BadRequestException(object),
-    ],
-    ['ClientError', (object): HttpException => new BadRequestException(object)],
-    [
-      'PermissionError',
-      (object): HttpException => new UnauthorizedException(object),
-    ],
-    [
-      'TokenNotValidError',
-      (object): HttpException => new UnauthorizedException(object),
-    ],
-    [
-      'TooManyTokensError',
-      (object): HttpException => new BadRequestException(object),
-    ],
-    [
-      'PermissionsUpdateInconsistentError',
-      (object): HttpException => new BadRequestException(object),
-    ],
-    [
-      'MediaBackendError',
-      (object): HttpException => new InternalServerErrorException(object),
-    ],
-    [
-      'PrimaryAliasDeletionForbiddenError',
-      (object): HttpException => new BadRequestException(object),
-    ],
-    [
-      'InvalidCredentialsError',
-      (object): HttpException => new UnauthorizedException(object),
-    ],
-    [
-      'NoLocalIdentityError',
-      (object): HttpException => new BadRequestException(object),
-    ],
   ]);
 
 @Catch()
@@ -76,7 +24,6 @@ export class ErrorExceptionMapping extends BaseExceptionFilter<Error> {
       error.name,
     );
     if (httpExceptionConstructor === undefined) {
-      // We don't know how to map this error and just leave it be
       return error;
     }
     const httpExceptionObject = buildHttpExceptionObject(
