7230647f8fa65ffa98c739214e309a46a0f17707
David Sánchez <deividsanchez96@hotmail.com>
2022-06-04 12:29:04 -0500

    fix import

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { Comment } from './entities/comment.entity';
 
 @Module({
   controllers: [CommentsController],
