f5d234f89c55bbcc13c3d4fa7bb5dd83d71bfa9f
David Sánchez <deividsanchez96@hotmail.com>
2022-06-04 16:28:26 -0500

    update create comment DTO

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCommentDto {}
+import { IsPositive, IsString } from 'class-validator';
+export class CreateCommentDto {
+  @IsPositive()
+  readonly user: number;
+
+  @IsPositive()
+  readonly task: number;
+
+  @IsString()
+  readonly text: string;
+}
